6d7eacead9a47b368097f66c98f194ff00b03d84
Aaron <Aaron.r.grau@gmail.com>
2016-04-25 09:41:00 -0400

    fix dragging animation

--- app/controllers/api/plays_controller.rb
+++ app/controllers/api/plays_controller.rb
@@ -7,12 +7,12 @@ class Api::PlaysController < ApplicationController
     @board = Board.new(board)
     @board.make_any_move(JSON.parse(params[:pos1]), JSON.parse(params[:pos2]))
     @mate = @board.is_mate?("black")
-    # unless @mate
-    #   computer = ComputerPlayer.new("black")
-    #   cpu_move = computer.play_turn(@board)
-    #   @board.make_any_move(cpu_move[0], cpu_move[1])
-    #   @mate = @board.is_mate?("white")
-    # end
+    unless @mate
+      computer = ComputerPlayer.new("black")
+      cpu_move = computer.play_turn(@board)
+      @board.make_any_move(cpu_move[0], cpu_move[1])
+      @mate = @board.is_mate?("white")
+    end
 
     render :new
   end
